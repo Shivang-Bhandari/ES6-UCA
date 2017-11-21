369266f70765e94f60e0184ddc7e9251fc6df2dc
Shivang Bhandari <pump382156@gmail.com>
2017-11-21 14:39:02 +0530

    Method Updates

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 // Populator function
 function getStudentDetails() {
   list.innerHTML="";
+
   students.map(student=> list.innerHTML+=`
       <li>
-        <div class="collapsible-header white"><span>${student.name}</span></div>
-        <div class="collapsible-body blue-grey lighten-3"><span>${student.batch}</span></div>
+
+      <div class="col s12 m6">
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">${student.name}</span>
+            <p>Roll Number : ${student.roll}</p>
+            <p>batch : ${student.batch}</p>
+            <p>passout : ${student.passout}</p>
+          </div>
+          <div class="card-action">
+            <a href="#" onclick=edit($(this))>Edit</a>
+            <a href="#" onclick=deleteStudent($(this))>Delete</a>
+          </div>
+        </div>
+      </div>
       </li>
       `);
 }
@@ -12,12 +26,16 @@ function getStudentDetails() {
 // Pre-Filled Object Array for students dummy data
 const students=[
   {
-    'name':'Abra Dabra',
-    'batch':'UCA2018'
+    'name':'Abra',
+    'roll' : '1510991999',
+    'batch':'UCA2018',
+    'passout' : '2019'
   },
   {
-    'name':'Fudu A',
-    'batch':'222'
+    'name':'Gujju',
+    'roll' : '141099888',
+    'passout' : '2018',
+    'batch':'CPM'
   }
 ];
 
@@ -27,18 +45,21 @@ let list=document.getElementById('main');
 const addStudent=()=>{
 
   let name=document.getElementById('add_name').value;
-  let lastName=document.getElementById('add_last_name').value;
   let batch=document.getElementById('add_batch').value;
-  name=`${name} ${lastName}`;
+  let roll=document.getElementById('add_roll').value;
+  let passout=document.getElementById('add_passout').value;
+
   // Check for fields
-  if(name==""||lastName==""||batch==""){
+  if(name==""||batch==""){
     Materialize.toast('Please Fill all form Feilds and Try Again', 3000, 'rounded');
   }
 
   // object decalaration
   const kid={
     name,
-    batch
+    batch,
+    roll,
+    passout
   };
 
   students.push(kid);
@@ -48,14 +69,7 @@ const addStudent=()=>{
 }
 
 // method to remove students
-const removeStudent = ()=>{
-  let name=document.getElementById('first_name').value;
-  let lastName=document.getElementById('last_name').value;
-  let batch=document.getElementById('batch').value;
-  let finalName=`${name} ${lastName}`;
-  if(name==""||lastName==""||batch==""){
-    Materialize.toast('Please Fill all form Feilds and Try Again', 3000, 'rounded');
-  }
-
-  
+const deleteStudent = ($element)=>{
+  let parent1 = $element.parent().parent();
+  parent1.remove();
 }
